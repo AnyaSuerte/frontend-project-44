61ed2cf5cdb2bba1787b1007c8269684ed6cc844
anyasuerte1 <anyasuerte@gmail.com>
2023-12-11 23:32:21 +0400

    add count

--- src/parity-game.js
+++ src/parity-game.js
@@ -1,36 +1,28 @@
 import readlineSync from 'readline-sync';
 
-const gameProcess = (userName) => {
+const gameProcess = (userName, count) => {
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
   console.log('Answer "yes" if the number is even, otherwise answer "no". ');
   const randomNumber = (Math.random() * 10 + 1).toFixed(0);
   console.log(`Question: ${randomNumber}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  // let numberOfCorrectAnswers = 0;
-  if (randomNumber === randomNumber % 2 || userAnswer === 'yes' || userAnswer === 'Yes') {
-    console.log('Correct!');
-    // numberOfCorrectAnswers += 1;
-    // console.log(numberOfCorrectAnswers);
-    gameProcess();
+  if ((randomNumber % 2) === 0 && (userAnswer === 'yes' || userAnswer === 'Yes')) {
+    console.log('10Correct!');
+    gameProcess(userName, count + 1);
+  } else if ((randomNumber % 2) !== 0 && (userAnswer === 'no' || userAnswer === 'No')) {
+    console.log('14Correct!');
+    gameProcess(userName, count + 1);
   } else {
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.`);
-    console.log(`Let's try again, ${userName}!`);
+    console.log('Неправильный ответ, сыграйте ещё раз');
+    gameProcess(userName, count);
   }
-  if (randomNumber !== randomNumber % 2 || userAnswer === 'no' || userAnswer === 'No') {
-    console.log('Correct!');
-    gameProcess();
-  // numberOfCorrectAnswers += 1;
-  // console.log(numberOfCorrectAnswers);
-  } else {
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
-    console.log(`Let's try again, ${userName}!`);
-  }
-// else if (numberOfCorrectAnswers === 3) {
-//   console.log(`Congratulations, ${userName}!`);
-// }
 };
 const parityGame = (userName) => {
   console.log(`Hello ${userName}!`);
-  gameProcess(userName);
+  gameProcess(userName, 0);
 };
 
 export default parityGame;
