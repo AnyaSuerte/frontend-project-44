8a05601d7aa6fb6d48a8739593d277949488301c
anyasuerte1 <anyasuerte@gmail.com>
2024-01-23 13:57:47 +0400

    add function getCorrectAnswer and fix the logic

--- src/parity-game.js
+++ src/parity-game.js
@@ -1,32 +1,29 @@
 import readlineSync from 'readline-sync';
 
+const getCorrectAnswer = (num) => {
+  if ((num % 2) === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
 const gameProcess = (userName, count) => {
-  console.log(count);
+  console.log('count', count);
   if (count === 3) {
     console.log(`Congratulations, ${userName}!`);
     return;
   }
-  console.log(count);
   console.log('Answer "yes" if the number is even, otherwise answer "no". ');
   const randomNumber = (Math.random() * 10 + 1).toFixed(0);
-  const isEvenNumber = (randomNumber % 2) === 0;
-  const isNotEvenNumber = (randomNumber % 2) !== 0;
+  const correctAnswer = getCorrectAnswer(randomNumber);
   console.log(`Question: ${randomNumber}`);
   const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-  if (isEvenNumber && (userAnswer !== 'yes')) {
-    console.log('1 иф');
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
-    console.log(`Let's try again, ${userName}!`);
-    gameProcess(userName, count);
-  } else if (isNotEvenNumber && (userAnswer !== 'no')) {
-    console.log('2 элси иф');
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
-    console.log(`Let's try again, ${userName}!`);
-    gameProcess(userName, count);
-  } else {
-    console.log('3 элси');
-    console.log('23Correct!');
+  if (correctAnswer === userAnswer) {
+    console.log('Correct!');
     gameProcess(userName, count + 1);
+  } else if (correctAnswer !== userAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    gameProcess(userName, 0);
   }
 };
 const parityGame = (userName) => {
