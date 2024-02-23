643f17d6802017b8959d3995d342dfda7396dea5
anyasuerte1 <anyasuerte@gmail.com>
2024-02-23 13:22:26 +0400

    fix path

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
-import calcGame from '../src/calc-game.js';
+import calcGame from '../src/games/calc-game.js';
 
 console.log(calcGame(userName));
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
-import parityGame from '../src/parity-game.js';
+import parityGame from '../src/games/parity-game.js';
 
 console.log(parityGame(userName));
